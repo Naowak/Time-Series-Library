3a222e008f11abb7e3e955eafaca44de1c809074
Naowak <yannis.bendiouis@gmail.com>
2025-08-28 16:45:02 +0200

    add run slurms

---
+++ slurms/run_slurms.sh
@@ -0,0 +1,6 @@
+sbatch slurms/anomaly_detection.sl
+sbatch slurms/classification.sl
+sbatch slurms/ETT-forecast.sl
+sbatch slurms/imputation.sl
+sbatch slurms/long_term_forecast.sl
+sbatch slurms/short_term_forecast.sl
