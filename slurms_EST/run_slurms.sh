966e0c662e2ba298addeba7266bbc45fa218c285
Naowak <yannis.bendiouis@gmail.com>
2025-11-21 16:49:08 +0100

    update slurms

---
+++ slurms_EST/run_slurms.sh
@@ -0,0 +1,6 @@
+sbatch slurms_EST/anomaly_detection.sl
+sbatch slurms_EST/classification.sl
+sbatch slurms_EST/ETT-forecast.sl
+sbatch slurms_EST/imputation.sl
+sbatch slurms_EST/long_term_forecast.sl
+sbatch slurms_EST/short_term_forecast.sl
